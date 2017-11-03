461988cb6465841e3dd9b801b6f9034405053e52
mehmetranas <mhmtranas@gamil.com>
2017-11-03 22:52:34 +0300

    fixed tarnsition problem

--- script.js
+++ script.js
@@ -10,22 +10,42 @@ $(document).ready(function () {
         }
     });
 
+    var quotes = [];
+
    $.ajax({
       method:'GET',
        url: "https://codepen.io/CzrNdlcc/pen/YrvZML.js",
        dataType:"json",
        success:function (data) {
-           console.log(data.quotes[0].content);
+           quotes = data.quotes;
        }
    });
 
-     $(".js-next").click(function () {
-         $(".quote").css("left","0");
-     });
 
-    $(".js-previous").click(function () {
-        $(".quote").css("left","-100%");
+   var index = 0;
+
+   var change = function (class1,class2,quote) {
+       if($("." + class2).length > 0){
+           $("." + class2).hide().removeClass(class2).addClass(class1 + " not-transition").show();
+       }
+
+       $("." + class1 + " p").text(quote.content);
+       $("." + class1 + " cite").text(quote.author);
+       $(".js-show").addClass(class2).removeClass("js-show");
+       $("." + class1).addClass("js-show").removeClass(class1 + " not-transition");
+   };
+
+    $(".js-next").click(function () {
+        var quote = quotes[index];
+        change("js-left","js-right",quote);
+        index++;
+        if(index>quotes.length-1) index = 0 ;
     });
 
-
+    $(".js-previous").click(function () {
+        var quote = quotes[index];
+       change("js-right","js-left",quote);
+       index--;
+       if(index<0) index = quotes.length-1;
+    });
 });
